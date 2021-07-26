66c90185b2444ff2052b46197807fab0a9609de1
Agnieszka Brown <abrown2012@icloud.com>
2021-07-26 11:08:10 -0400

    commit tree 95287821049e8b11a7ecc9d241184e99c0beeb7e

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React from 'react'
+
+
+const test = (props) => {
+    return (
+        <div className="review">
+            {props.display_title}
+        </div>
+    )        
+}
+
+
+const MovieReviews = ({reviews})=> {
+    
+
+
+    return (
+        <div className="review-list">
+            {reviews.map(test)}
+        </div>
+    )
+}
+
+export default MovieReviews;
